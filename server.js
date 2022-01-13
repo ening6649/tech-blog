2e94448b9654b8b8e5064c7b001636277416002c
ening <ening6649@yahoo.com>
2022-01-12 19:28:59 -0500

    added routes

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection')
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+// helpers util implementation
+const helpers = require('./utils/helpers');
+// helpers util implementation
+const hbs = exphbs.create({ helpers });
+// template engine handlebar middle ware
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+// the below makes the css style sheet avaiable to the client
+// makes everything in the public folder available 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
-// sync sequelize models to the database, then turn on the server
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
